f299ac15adae4a2cce5cd1a07b050638365776ed
tomazickarin1 <karin.tomazic@agiledrop.com>
2026-03-12 14:00:23 +0100

    ESlint task

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,7 @@
 import eslint from '@eslint/js';
 import { defineConfig } from 'eslint/config';
 import tseslint from 'typescript-eslint';
+import prettierConfig from 'eslint-config-prettier';
 
 export default defineConfig(
   eslint.configs.recommended,
@@ -14,6 +15,5 @@ export default defineConfig(
       },
     },
   },
+  prettierConfig,
 );
-
-
