11e4e41ce5f7d0b9ca468b9df48a149a5fd72562
tomazickarin1 <karin.tomazic@agiledrop.com>
2026-03-19 10:03:43 +0100

    menu link component - mobile design

--- src/components/MenuLink.tsx
+++ src/components/MenuLink.tsx
@@ -1,10 +1,21 @@
-import styles from './MenuLink.module.scss'
+import styles from "./MenuLink.module.scss";
 
 export interface MenuLinkProps {
   label: string;
   url: string;
 }
 
-export default function MenuLink({label, url}:MenuLinkProps) {
-  return <a className={styles.link} aria-label={label} href={url}>{label}</a>
+export default function MenuLink({ label, url }: MenuLinkProps) {
+  return (
+    <a
+      className={styles.link}
+      aria-label={label}
+      href={url}
+      onClick={(e) => {
+        e.preventDefault();
+      }}
+    >
+      {label}
+    </a>
+  );
 }
