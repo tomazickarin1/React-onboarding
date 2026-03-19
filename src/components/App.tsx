6ac846a7a7a139d3494996000bd1f859a90f4055
tomazickarin1 <karin.tomazic@agiledrop.com>
2026-03-19 10:34:53 +0100

    menu link list component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,13 @@
 // functional component
-import MenuLink from "./MenuLink";
+import MenuLinkList from "./MenuLinkList";
 import styles from './App.module.scss'
+
+const links = [
+  { label: "Home", url: "/home" },
+  { label: "About", url: "/about" },
+  { label: "Contact", url: "/contact" }
+]
+
 export interface AppProps {
   text: string;
 }
@@ -9,7 +16,7 @@ export default function App({text}:AppProps) {
   return (
     <div className={styles.movieapp}>
       {text}
-      <MenuLink label={'label test'} url={'url test'}/>
+      <MenuLinkList links={links} ></MenuLinkList>
     </div>
   );
 }
---
+++ src/components/MenuLinkList.tsx
@@ -0,0 +1,20 @@
+import styles from "./MenuLinkList.module.scss";
+import MenuLink from "./MenuLink";
+
+export interface MenuLinkListProps {
+  links: Array<{ label: string; url: string }>;
+}
+
+export default function MenuLinkList({ links }: MenuLinkListProps) {
+  return (
+    <div className={styles.menuWrapper}>
+      <ul className={styles.menu}>
+        {links.map((link) => (
+          <li key={link.url}>
+            <MenuLink label={link.label} url={link.url}></MenuLink>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
