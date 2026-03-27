68b374107351c0a77b2b8ece1ba394bc9acb915d
tomazickarin1 <karin.tomazic@agiledrop.com>
2026-03-27 12:57:51 +0100

    refactor language select to input with filtering

--- src/components/languageSwitcher/LanguageSelect.tsx
+++ src/components/languageSwitcher/LanguageSelect.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 export interface LanguageSelectProps {
   languageList: Array<{ code: string; label: string }>;
   selected: string;
@@ -11,20 +12,68 @@ export default function LanguageSelect({
   onSelect,
   type,
 }: LanguageSelectProps) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [filter, setFilter] = useState("");
+
+  const handleOpen = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleFilter = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFilter(e.target.value);
+  };
+
+  // const listLabels = (list: { code: string; label: string }) => {
+  //   return list.label.toLowerCase().includes(filter.toLowerCase());
+  // };
+
+  // const filterList = () => {
+  //   return languageList.filter(listLabels);
+  // };
+
+  // const searchedList = (list: { code: string; label: string }) => {
+  //   return () => {
+  //     onSelect(list.code, type);
+  //   };
+  // };
+
+  const renderedList = () => {
+    return languageList
+      .filter((list) => list.label.toLowerCase().includes(filter.toLowerCase()))
+      .map((list) => {
+        return (
+          <li key={list.code} onMouseDown={() => { onSelect(list.code, type); }}>{list.label}</li>
+        );
+      });
+  };
+
   return (
-    <select
-      name="Default Language"
-      id="defaultLanguage"
-      value={selected}
-      onChange={(e) => {
-        onSelect(e.target.value, type);
-      }}
-    >
-      {languageList.map((list) => (
-        <option key={list.code} value={list.code}>
-          {list.label}
-        </option>
-      ))}
-    </select>
+    <>
+      <button type="button" onClick={handleOpen}>
+        {selected}
+      </button>
+
+      {isOpen && (
+        <div>
+          <input type="text" value={filter} onChange={handleFilter} />
+          <ul>{renderedList()}</ul>
+        </div>
+      )}
+
+      <select
+        name="Default Language"
+        id="defaultLanguage"
+        value={selected}
+        onChange={(e) => {
+          onSelect(e.target.value, type);
+        }}
+      >
+        {languageList.map((list) => (
+          <option key={list.code} value={list.code}>
+            {list.label}
+          </option>
+        ))}
+      </select>
+    </>
   );
 }
--- src/components/languageSwitcher/LanguageSwitcher.tsx
+++ src/components/languageSwitcher/LanguageSwitcher.tsx
@@ -1,6 +1,6 @@
 import styles from "./LanguageSwitcher.module.scss";
 import { useState, useEffect, useRef } from "react";
-// import LanguageSelect from "./LanguageSelect";
+import LanguageSelect from "./LanguageSelect";
 
 export interface LanguageSwitcherProps {
   languageList: Array<{ code: string; label: string }>;
@@ -39,9 +39,9 @@ export default function LanguageSwitcher({
     setIsOpen(!isOpen);
   };
 
-  const handleSelect = (e: React.ChangeEvent<HTMLSelectElement>, type: "primary" | "fallback") => {
-    onSelect(e.target.value, type);
-  };
+  // const handleSelect = (e: React.ChangeEvent<HTMLSelectElement>, type: "primary" | "fallback") => {
+  //   onSelect(e.target.value, type);
+  // };
 
   return (
     <section className={styles.languageSwitcher} ref={ref}>
@@ -60,7 +60,7 @@ export default function LanguageSwitcher({
                     Reset
                   </button>
                 </div>
-                <select
+                {/* <select
                   name="Default Language"
                   id="defaultLanguage"
                   value={selectedMain}
@@ -71,13 +71,13 @@ export default function LanguageSwitcher({
                       {list.label}
                     </option>
                   ))}
-                </select>
-                {/* <LanguageSelect languageList={languageList} selected={selectedMain} type="primary" onSelect={onSelect} /> */}
+                </select> */}
+                <LanguageSelect languageList={languageList} selected={selectedMain} type="primary" onSelect={onSelect} />
               </div>
 
               <div>
                 <label htmlFor="fallbackLanguage">Fallback Language</label>
-                <select
+                {/* <select
                   name=""
                   id="fallbackLanguage"
                   value={selectedFallback}
@@ -88,8 +88,8 @@ export default function LanguageSwitcher({
                       {list.label}
                     </option>
                   ))}
-                </select>
-                {/* <LanguageSelect languageList={languageList} selected={selectedFallback} type="fallback" onSelect={onSelect} /> */}
+                </select> */}
+                <LanguageSelect languageList={languageList} selected={selectedFallback} type="fallback" onSelect={onSelect} />
               </div>
             </fieldset>
           </form>
