5159d9e8d4e54f2c645d9ac88f2ad86a4d4f1e07
tomazickarin1 <karin.tomazic@agiledrop.com>
2026-04-02 14:52:51 +0200

    Testing primer - third and fourth test

--- src/components/languageSwitcher/LanguageSwitcher.test.tsx
+++ src/components/languageSwitcher/LanguageSwitcher.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import LanguageSwitcher from "./LanguageSwitcher";
 import userEvent from "@testing-library/user-event";
 
@@ -18,7 +18,6 @@ describe("LanguageSwitcher", () => {
         onReset={() => {}}
       />,
     );
-
     const button = screen.getByRole("button", { name: /language settings/i });
     expect(button).toHaveTextContent("US");
   });
@@ -39,9 +38,53 @@ describe("LanguageSwitcher", () => {
     const button = screen.getByRole("button", { name: /language settings/i });
     await user.click(button);
 
-    expect(screen.getByRole('group', { name: /language preferences/i })).toBeInTheDocument();
-    expect(screen.getByRole('button', { name: /reset/i })).toBeInTheDocument();
-    expect(screen.getByRole('button', { name: /english/i })).toBeInTheDocument();
-    expect(screen.getByRole('button', { name: /german/i })).toBeInTheDocument();
+    expect(screen.getByRole("group", { name: /language preferences/i })).toBeInTheDocument();
+    expect(screen.getByRole("button", { name: /reset/i })).toBeInTheDocument();
+    expect(screen.getByRole("button", { name: /english/i })).toBeInTheDocument();
+    expect(screen.getByRole("button", { name: /german/i })).toBeInTheDocument();
   });
+
+  test("Clicking on language switcher's main button while pick box is open keeps it open.", async () => {
+    const user = userEvent.setup();
+    render(
+      <LanguageSwitcher
+        languageList={mockLnaguagelist}
+        selectedMain="en-US"
+        selectedFallback="de-DE"
+        onSelect={() => {}}
+        onReset={() => {}}
+      />,
+    );
+    const button = screen.getByRole("button", { name: /language settings/i });
+    // Click twice
+    await user.click(button);
+    await user.click(button);
+    expect(screen.getByRole("group", { name: /language preferences/i })).toBeInTheDocument();
+  });
+
+
+  test("Clicking outside the langue switcher closes the pick box", async () => {
+    const user = userEvent.setup();
+    render(
+      <LanguageSwitcher
+        languageList={mockLnaguagelist}
+        selectedMain="en-US"
+        selectedFallback="de-DE"
+        onSelect={() => {}}
+        onReset={() => {}}
+      />,
+    );
+
+    const button = screen.getByRole("button", { name: /language settings/i });
+    // open
+    await user.click(button);
+
+    expect(screen.getByRole("group", { name: /language preferences/i })).toBeInTheDocument();
+
+    fireEvent.mouseDown(document.body);
+
+    expect(screen.queryByRole("group", {name: /language preferences/i})).not.toBeInTheDocument();
+  });
+
+
 });
