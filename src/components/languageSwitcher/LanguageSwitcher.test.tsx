0bd43e6e64c94e03765052ece09c3fa7b663c84a
tomazickarin1 <karin.tomazic@agiledrop.com>
2026-04-02 15:54:24 +0200

    Testing primer - add 5th test and add beforeEach to reduce duplications

--- src/components/languageSwitcher/LanguageSwitcher.test.tsx
+++ src/components/languageSwitcher/LanguageSwitcher.test.tsx
@@ -8,7 +8,8 @@ const mockLnaguagelist = [
 ];
 
 describe("LanguageSwitcher", () => {
-  test("Language switchers main button shows appropriate language information", () => {
+
+  beforeEach(() => {
     render(
       <LanguageSwitcher
         languageList={mockLnaguagelist}
@@ -16,25 +17,18 @@ describe("LanguageSwitcher", () => {
         selectedFallback="de-DE"
         onSelect={() => {}}
         onReset={() => {}}
-      />,
+      />
     );
+  });
+
+
+  test("Language switchers main button shows appropriate language information", () => {
     const button = screen.getByRole("button", { name: /language settings/i });
     expect(button).toHaveTextContent("US");
   });
 
   test("Clicking on language switcher's main button displays the pick box with the controls.", async () => {
     const user = userEvent.setup();
-
-    render(
-      <LanguageSwitcher
-        languageList={mockLnaguagelist}
-        selectedMain="en-US"
-        selectedFallback="de-DE"
-        onSelect={() => {}}
-        onReset={() => {}}
-      />,
-    );
-
     const button = screen.getByRole("button", { name: /language settings/i });
     await user.click(button);
 
@@ -46,15 +40,6 @@ describe("LanguageSwitcher", () => {
 
   test("Clicking on language switcher's main button while pick box is open keeps it open.", async () => {
     const user = userEvent.setup();
-    render(
-      <LanguageSwitcher
-        languageList={mockLnaguagelist}
-        selectedMain="en-US"
-        selectedFallback="de-DE"
-        onSelect={() => {}}
-        onReset={() => {}}
-      />,
-    );
     const button = screen.getByRole("button", { name: /language settings/i });
     // Click twice
     await user.click(button);
@@ -65,15 +50,6 @@ describe("LanguageSwitcher", () => {
 
   test("Clicking outside the langue switcher closes the pick box", async () => {
     const user = userEvent.setup();
-    render(
-      <LanguageSwitcher
-        languageList={mockLnaguagelist}
-        selectedMain="en-US"
-        selectedFallback="de-DE"
-        onSelect={() => {}}
-        onReset={() => {}}
-      />,
-    );
 
     const button = screen.getByRole("button", { name: /language settings/i });
     // open
@@ -84,6 +60,22 @@ describe("LanguageSwitcher", () => {
     fireEvent.mouseDown(document.body);
 
     expect(screen.queryByRole("group", {name: /language preferences/i})).not.toBeInTheDocument();
+  });
+
+
+  test("Pick box has appropriate elements displayed", async () => {
+    const user = userEvent.setup();
+
+    const button = screen.getByRole("button", { name: /language settings/i });
+    await user.click(button);
+
+    expect(screen.getByRole("group", { name: /language preferences/i })).toBeInTheDocument();
+    expect(screen.getByText(/default language/i)).toBeInTheDocument();
+    expect(screen.getByText(/fallback language/i)).toBeInTheDocument();
+    expect(screen.getByRole("button", { name: /reset/i })).toBeInTheDocument();
+    expect(screen.getByRole("button", { name: /english/i })).toBeInTheDocument();
+    expect(screen.getByRole("button", { name: /german/i })).toBeInTheDocument();
+
   });
 
 
