a9c89e7ef3586ceee5c660949a7d796200063db9
tomazickarin1 <karin.tomazic@agiledrop.com>
2026-04-08 09:46:47 +0200

    update navigation markup and styling

--- src/components/navigation/NavigationBar.tsx
+++ src/components/navigation/NavigationBar.tsx
@@ -8,28 +8,43 @@ import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 export default function NavigationBar() {
   return (
-    <nav className={styles.navbar}>
-      <div className={styles.left}>
-        <a href="/" className={styles.logo} aria-label="Home">
-          <img src={logo} />
-        </a>
-        <MainMenu />
+    <>
+      <div className={styles.navbarWrapper}>
+        <nav className={styles.navbar}>
+          <div className={styles.left}>
+            <a href="/" className={styles.logo} aria-label="Home">
+              <img src={logo} />
+            </a>
+            <MainMenu />
+          </div>
+          <div className={styles.right}>
+            <a
+              href=""
+              aria-label="Create New Movie or TV Show"
+              className={styles.plus}
+            >
+              <img src={plus} />
+            </a>
+            <LanguageSwitcherHandler />
+            <a href="#">Login link</a>
+            <a href="#">Join link</a>
+            <div className={styles.serchIcon}>
+              <FontAwesomeIcon icon={faMagnifyingGlass} aria-hidden="true" />
+            </div>
+          </div>
+        </nav>
       </div>
-      <div className={styles.right}>
-        <a
-          href=""
-          aria-label="Create New Movie or TV Show"
-          className={styles.plus}
-        >
-          <img src={plus} />
-        </a>
-        <LanguageSwitcherHandler />
-        <a href="#">Login link</a>
-        <a href="#">Join link</a>
-        <div className={styles.serchIcon}>
+
+      <div className={styles.searchBarWrapper}>
+        <form className={styles.searchBar} role="search">
           <FontAwesomeIcon icon={faMagnifyingGlass} aria-hidden="true" />
-        </div>
+          <input
+            type="search"
+            aria-label="Search for a movie, tv show, person"
+            placeholder="Search for a movie, tv show, person..."
+          />
+        </form>
       </div>
-    </nav>
+    </>
   );
 }
