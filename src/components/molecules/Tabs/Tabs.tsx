c46ed5ed6084352c2d4e67596d6859e0f672eab1
tomazickarin1 <karin.tomazic@agiledrop.com>
2026-04-17 11:36:55 +0200

    update tabs mobile styling

--- src/components/molecules/Tabs/Tabs.tsx
+++ src/components/molecules/Tabs/Tabs.tsx
@@ -1,6 +1,8 @@
 import styles from "./Tabs.module.scss";
 import Tab from "../../atoms/Tab/Tab";
 import { useRef, useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 
 interface TabsProps {
   tabs: Array<{ id: number; label: string }>;
@@ -26,9 +28,9 @@ export default function Tabs({ tabs, activeTab, onTabChange }: TabsProps) {
     });
   }, [activeTab, tabs]);
 
-  const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    onTabChange(Number(e.target.value));
-  };
+  // const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+  //   onTabChange(Number(e.target.value));
+  // };
 
   // const handleOpen = () => {
   //   setIsOpen(!isOpen);
@@ -48,23 +50,36 @@ export default function Tabs({ tabs, activeTab, onTabChange }: TabsProps) {
               <li
                 key={tab.id}
                 role="option"
+                tabIndex={0}
+                className={tab.id === activeTab ? styles.activeOption : ""}
                 aria-selected={tab.id === activeTab}
+                onKeyDown={(e) => {
+                  if (e.key === "Enter") {
+                    onTabChange(tab.id);
+                    setIsOpen(false);
+                  }
+                }}
                 onClick={() => {
                   onTabChange(tab.id);
                   setIsOpen(false);
                 }}
               >
                 {tab.id === activeTab ? (
-                  <button
-                    type="button"
-                    className={styles.mobileDropdownTrigger}
-                    onClick={(e) => {
-                      e.stopPropagation();
-                      setIsOpen(false);
-                    }}
-                  >
-                    {tab.label} <span className={styles.iconDown}></span>
-                  </button>
+                  <div className={styles.activeBtnWrapper}>
+                    <button
+                      type="button"
+                      className={styles.mobileDropdownTrigger}
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        setIsOpen(false);
+                      }}
+                    >
+                      {tab.label}{" "}
+                      <span className={styles.iconDown}>
+                        <FontAwesomeIcon icon={faChevronDown} />
+                      </span>
+                    </button>
+                  </div>
                 ) : (
                   tab.label
                 )}
@@ -74,65 +89,23 @@ export default function Tabs({ tabs, activeTab, onTabChange }: TabsProps) {
         )}
 
         {!isOpen && (
-          <button
-            type="button"
-            className={styles.mobileDropdownTrigger}
-            onClick={() => {
-              setIsOpen(true);
-            }}
-            aria-haspopup="listbox"
-          >
-            {tabs.find((tab) => tab.id === activeTab)?.label}
-            <span className={styles.iconDown}></span>
-          </button>
+          <div className={styles.activeBtnWrapper}>
+            <button
+              type="button"
+              className={styles.mobileDropdownTrigger}
+              onClick={() => {
+                setIsOpen(true);
+              }}
+              aria-haspopup="listbox"
+            >
+              {tabs.find((tab) => tab.id === activeTab)?.label}
+              <span className={styles.iconDown}>
+                <FontAwesomeIcon icon={faChevronDown} />
+              </span>
+            </button>
+          </div>
         )}
       </div>
-
-      {/* <div
-        className={styles.mobileDropdown}
-        role="listbox"
-        aria-label="select category"
-      >
-        <button
-          type="button"
-          className={styles.mobileDropdownTrigger}
-          onClick={handleOpen}
-          aria-haspopup="listbox"
-        >
-          {tabs.find((tab) => tab.id === activeTab)?.label}
-          <span className={styles.iconDown}></span>
-        </button>
-
-        {isOpen && (
-          <ul className={styles.dropdownWrapper}>
-            {tabs.map((tab) => (
-              <li
-                key={tab.id}
-                role="option"
-                aria-selected={tab.id === activeTab}
-                className={tab.id === activeTab ? styles.activeOption : ""}
-                onClick={() => {handleSelect(tab.id)}}
-              >
-                {tab.label}
-              </li>
-            ))}
-          </ul>
-        )}
-      </div> */}
-
-      <select
-        value={activeTab}
-        onChange={handleSelectChange}
-        className={styles.mobileSelect}
-      >
-        {tabs.map((tab) => {
-          return (
-            <option key={tab.id} value={tab.id}>
-              {tab.label}
-            </option>
-          );
-        })}
-      </select>
 
       <div className={styles.selectorWrap} ref={wrapperRef}>
         <div
