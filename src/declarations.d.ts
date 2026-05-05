7a470ef2934b02e608462009f7187cb31737d927
tomazickarin1 <karin.tomazic@agiledrop.com>
2026-05-05 13:32:46 +0200

    create and use showcase fetch component

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,3 +1,11 @@
+interface ImportMetaEnv {
+  readonly VITE_TMDB_API_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
 declare module '*.svg' {
   const src: string;
   export default src;
