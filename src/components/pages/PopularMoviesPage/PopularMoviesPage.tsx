a859fb2ffa1622fa653839742ed2849c22c921d9
Karin <karin.tomazic@agiledrop.com>
2026-07-01 12:18:15 +0200

    Add sort and genre filter panels to PopularMoviesPage

--- src/components/pages/PopularMoviesPage/PopularMoviesPage.tsx
+++ src/components/pages/PopularMoviesPage/PopularMoviesPage.tsx
@@ -6,6 +6,7 @@ import Icon from "../../atoms/Icon/Icon";
 import { useQuery } from "@tanstack/react-query";
 import {
   faChevronRight,
+  faChevronDown,
   faCaretDown,
 } from "@fortawesome/free-solid-svg-icons";
 import { useClickOutside } from "../../../hooks/useClickOutside";
@@ -13,7 +14,10 @@ import { useClickOutside } from "../../../hooks/useClickOutside";
 const tmbUrl = "https://api.themoviedb.org/3";
 const tmbImageUrl = "https://image.tmdb.org/t/p/w500";
 
-const DEFAULT_SORT = { value: "popularity.desc", label: "Popularity Descending" };
+const DEFAULT_SORT = {
+  value: "popularity.desc",
+  label: "Popularity Descending",
+};
 
 const sortOptions = [
   DEFAULT_SORT,
@@ -34,6 +38,8 @@ interface Tmdbmovie {
 }
 type Movie = { id: number; url: string; title: string; date: string };
 
+type Genre = { id: number; name: string };
+
 async function fetchPopularMovies(): Promise<Movie[]> {
   const apiKey = import.meta.env.VITE_TMDB_API_KEY;
   const response = await fetch(`${tmbUrl}/movie/popular?api_key=${apiKey}`);
@@ -43,6 +49,7 @@ async function fetchPopularMovies(): Promise<Movie[]> {
   }
 
   const data = (await response.json()) as { results: Tmdbmovie[] };
+
   // console.log(data);
 
   return data.results.map((movie) => ({
@@ -53,93 +60,147 @@ async function fetchPopularMovies(): Promise<Movie[]> {
   }));
 }
 
-// fetchPopularMovies();
+async function getGenres(): Promise<Genre[]> {
+  const apiKey = import.meta.env.VITE_TMDB_API_KEY;
+  const response = await fetch(`${tmbUrl}/genre/movie/list?api_key=${apiKey}`);
+
+  if (!response.ok) {
+    throw new Error(`Request failed with status ${String(response.status)}`);
+  }
+
+  const data = (await response.json()) as { genres: Genre[] };
+
+  return data.genres;
+}
 
 export default function PopularMovies() {
-  const { data } = useQuery({
+  const { data: movies } = useQuery({
     queryKey: ["popular-movies-page"],
     queryFn: fetchPopularMovies,
   });
+
+  const { data: genre } = useQuery({
+    queryKey: ["genre-list"],
+    queryFn: getGenres,
+  });
+
+  console.log(genre);
 
   const [isSortOpen, setIsSortOpen] = useState(false);
   const [sortBy, setSortBy] = useState(DEFAULT_SORT);
   const sortRef = useRef<HTMLDivElement>(null);
   const isClickedOutsideSort = useClickOutside(sortRef);
 
-  const handleSortToggle = () => {
+  const handleSortOpen = () => {
     const isCurrentlyOpen = isSortOpen && !isClickedOutsideSort;
     setIsSortOpen(!isCurrentlyOpen);
   };
 
+  const [sortToggle, setSortToggle] = useState(false);
+
+  const handleSortToggle = () => {
+    console.log("toggle");
+    setSortToggle(!sortToggle);
+  };
+
   return (
     <SingleColumn>
-      <div className={styles.popularWrapper}>
-        <div>
-          <h2>Popular Movies</h2>
-          <div className={styles.filterWrapper}>
-            <div className={styles.filterPanel}>
-              <div className={styles.name}>
-                <h2>Sort</h2>
-                <Icon icon={faChevronRight} className={styles.chevron ?? ""} />
-              </div>
-              <div className={styles.filter}>
-                <h3>Sort Results By</h3>
-                <div className={styles.sortWrapper} ref={sortRef}>
-                  <button
-                    type="button"
-                    onClick={handleSortToggle}
-                    className={styles.sortBtn}
-                    aria-expanded={isSortOpen}
-                    aria-haspopup="listbox"
-                  >
-                    {sortBy.label}
-                    <Icon icon={faCaretDown} className={styles.caret ?? ""} />
-                  </button>
+      <div className={styles.container}>
+        <h2>Popular Movies</h2>
+        <div className={styles.popularWrapper}>
+          <div>
+            <div className={styles.filterWrapper}>
+              <div className={styles.filterPanel}>
+                <div className={styles.sortName} onClick={handleSortToggle}>
+                  <h3>Sort</h3>
 
-                  {isSortOpen && !isClickedOutsideSort && (
-                    <ul className={styles.sortList} role="listbox">
-                      {sortOptions.map((option) => (
-                        <li
-                          key={option.value}
-                          role="option"
-                          tabIndex={0}
-                          aria-selected={option.value === sortBy.value}
-                          className={
-                            option.value === sortBy.value
-                              ? styles.active
-                              : ""
-                          }
-                          onMouseDown={(e) => {
-                            e.stopPropagation();
-                            setSortBy(option);
-                            setIsSortOpen(false);
-                          }}
-                        >
-                          {option.label}
-                        </li>
-                      ))}
-                    </ul>
+                  {sortToggle ? (
+                    <Icon
+                      icon={faChevronDown}
+                      className={styles.chevron ?? ""}
+                    />
+                  ) : (
+                    <Icon
+                      icon={faChevronRight}
+                      className={styles.chevron ?? ""}
+                    />
                   )}
+                </div>
+
+                {sortToggle && (
+                  <div className={styles.sortFilter}>
+                    <h3>Sort Results By</h3>
+                    <div className={styles.sortWrapper} ref={sortRef}>
+                      <button
+                        type="button"
+                        onClick={handleSortOpen}
+                        className={styles.sortBtn}
+                        aria-expanded={isSortOpen}
+                        aria-haspopup="listbox"
+                      >
+                        {sortBy.label}
+                        <Icon
+                          icon={faCaretDown}
+                          className={styles.caret ?? ""}
+                        />
+                      </button>
+
+                      {isSortOpen && !isClickedOutsideSort && (
+                        <ul className={styles.sortList} role="listbox">
+                          {sortOptions.map((option) => (
+                            <li
+                              key={option.value}
+                              role="option"
+                              tabIndex={0}
+                              aria-selected={option.value === sortBy.value}
+                              className={
+                                option.value === sortBy.value
+                                  ? styles.active
+                                  : ""
+                              }
+                              onMouseDown={(e) => {
+                                e.stopPropagation();
+                                setSortBy(option);
+                                setIsSortOpen(false);
+                              }}
+                            >
+                              {option.label}
+                            </li>
+                          ))}
+                        </ul>
+                      )}
+                    </div>
+                  </div>
+                )}
+              </div>
+              <div className={styles.filterPanel}>
+                <div className={styles.genresName}>
+                  <h3>Filters</h3>
+                  <Icon icon={faChevronDown} className={styles.chevron ?? ""} />
+                </div>
+                <div className={styles.genresFilter}>
+                  <p>Genres</p>
+                  <ul></ul>
+                  {genre?.map((g) => {
+                    return <li key={g.id}>{g.name}</li>;
+                  })}
                 </div>
               </div>
             </div>
-            <div>
-              <p>filters</p>
-            </div>
           </div>
-        </div>
-        <div className={styles.moviesGrid}>
-          {data?.map((r) => {
-            return (
-              <Card
-                key={r.id}
-                image={r.url}
-                title={r.title}
-                date={r.date}
-                variant="popular"
-              />
-            );
-          })}
+          <div className={styles.moviesGrid}>
+            {movies?.map((r) => {
+              return (
+                <Card
+                  key={r.id}
+                  image={r.url}
+                  title={r.title}
+                  date={r.date}
+                  variant="popular"
+                />
+              );
+            })}
+          </div>
         </div>
       </div>
     </SingleColumn>
