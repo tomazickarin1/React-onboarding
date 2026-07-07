5621f7449bebe22fff8f8b892ad476111c42be45
Karin <karin.tomazic@agiledrop.com>
2026-07-07 10:16:01 +0200

    create a new iconButton story

---
+++ src/components/atoms/IconButton/IconButton.stories.tsx
@@ -0,0 +1,34 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
+import IconButtonComponent from "./IconButton";
+
+import {
+  faList,
+  faHeart,
+  faBookmark
+} from "@fortawesome/free-solid-svg-icons";
+
+const meta = {
+  title: "Component/atoms/IconButton",
+  component: IconButtonComponent,
+} satisfies Meta<typeof IconButtonComponent>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const List: Story = {
+  args: {
+    icon: faList
+  },
+};
+
+export const Heart: Story = {
+  args: {
+    icon: faHeart
+  },
+};
+
+export const Bookmark: Story = {
+  args: {
+    icon: faBookmark
+  },
+};
