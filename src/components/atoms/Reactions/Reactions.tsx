d8e32b8f017b0e6b634a88ba1243543e842e05de
Karin <karin.tomazic@agiledrop.com>
2026-07-13 15:58:27 +0200

    Add vibe-button tooltip and extract reactions into their own component

---
+++ src/components/atoms/Reactions/Reactions.tsx
@@ -0,0 +1,14 @@
+import styles from "./Reactions.module.scss";
+import emoji1 from "../../../assets/emoji1.svg";
+import emoji2 from "../../../assets/emoji2.svg";
+import emoji3 from "../../../assets/emoji3.svg";
+
+export default function Reactions() {
+  return (
+    <div className={styles.reactions}>
+      <div><img src={emoji1} alt="" /></div>
+      <div><img src={emoji2} alt="" /></div>
+      <div><img src={emoji3} alt="" /></div>
+    </div>
+  );
+}
--- src/components/molecules/UserScore/UserScore.tsx
+++ src/components/molecules/UserScore/UserScore.tsx
@@ -2,11 +2,12 @@ import styles from "./UserScore.module.scss";
 import { faCircleInfo } from "@fortawesome/free-solid-svg-icons";
 import ScoreCircle from "../../atoms/ScoreCircle/ScoreCircle";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Reactions from "../../atoms/Reactions/Reactions";
 
 type ScoreProps = { score: number };
 
 export default function UserScore({ score }: ScoreProps) {
-  const userScore = Math.round(score * 10);
+  const userScore = Math.max(0, Math.min(100, Math.round(score * 10)));
   return (
     <div className={styles.scoreWrapper}>
       <ScoreCircle score={userScore} />
@@ -15,13 +16,15 @@ export default function UserScore({ score }: ScoreProps) {
         <br />
         Score
       </span>
-      <div className={styles.reactions}>
-        <span>😍</span>
-        <span>😆</span>
-        <span>🥲</span>
-      </div>
+      <Reactions />
       <button className={styles.vibeButton}>
-        What&apos;s your Vibe? <FontAwesomeIcon icon={faCircleInfo} />
+        What&apos;s your Vibe?
+        <span className={styles.tooltipWrapper}>
+          <FontAwesomeIcon icon={faCircleInfo} />
+          <span className={styles.tooltip}>
+            Login to use TMDBs new rating system.
+          </span>
+        </span>
       </button>
     </div>
   );
