7ebd2693dbb3ed2ef1a1d48ccd9bceb5b05208b3
Karin <karin.tomazic@agiledrop.com>
2026-07-27 15:49:03 +0200

    switch CSS module IntelliSense to typescript-plugin-css-modules

--- src/components/atoms/Reactions/Reactions.module.scss.d.ts
+++
@@ -1,9 +0,0 @@
-export type Styles = {
-  reactions: string;
-};
-
-export type ClassNames = keyof Styles;
-
-declare const styles: Styles;
-
-export default styles;
--- src/components/molecules/GenreFilter/GenreFilter.module.scss.d.ts
+++
@@ -1,9 +0,0 @@
-export type Styles = {
-  genreList: string;
-};
-
-export type ClassNames = keyof Styles;
-
-declare const styles: Styles;
-
-export default styles;
