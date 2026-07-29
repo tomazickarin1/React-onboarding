e07155136ea41e7e26113d71f385f8bc5841b8ae
Karin <karin.tomazic@agiledrop.com>
2026-07-29 12:30:24 +0200

    remove Chromatic and addon-onboarding from Storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,11 +6,9 @@ const config: StorybookConfig = {
     "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
   "addons": [
-    "@chromatic-com/storybook",
     "@storybook/addon-vitest",
     "@storybook/addon-a11y",
     "@storybook/addon-docs",
-    "@storybook/addon-onboarding"
   ],
   "framework": "@storybook/react-vite"
 };
